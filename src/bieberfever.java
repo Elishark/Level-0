75a3cab295397563703d471254453fcce1e3c9cc
rylr <league@WTS9.attlocal.net>
2016-10-11 16:58:38 -0700

    stuff

---
+++ src/bieberfever.java
@@ -0,0 +1,16 @@
+
+public class bieberfever {
+	public static void main(String[] args) {
+		baby("oh");
+		baby("no");
+		baby("oh");
+		System.out.println("I thought you'd allways be mine!");
+	}
+
+	public static void baby(String input) {
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Baby");
+		}
+		System.out.println(input);
+	}
+}
