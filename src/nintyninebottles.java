c3acf1a3ca2cc4941f3e162ac323dd514000b626
Stephen Cerruti <scerruti@otabi.com>
2017-02-14 16:57:54 -0800

    asdfasdf

--- src/nintyninebottles.java
+++ src/nintyninebottles.java
@@ -2,8 +2,8 @@ import javax.swing.JOptionPane;
 
 public class nintyninebottles {
 public static void main(String[] args) {
-	for(int j=99; j>0; j--){
-	JOptionPane.showMessageDialog(null, j + " bottles of beer on the wall " + j + " bottles of beer. Take on down pass it around " + (j-1) + " bottles the wall");
+	for(int j=0; 99>j; j++){
+	JOptionPane.showMessageDialog(null, (99-j) + " bottles of beer on the wall " + (99-j) + " bottles of beer. Take on down pass it around " + (99-j-1) + " bottles the wall");
 	}
 	
 }
